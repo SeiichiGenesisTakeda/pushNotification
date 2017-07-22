00f49b0420ab13db0acf7b8beb82d0c955f72aa0
SeiichiGenesisTakeda <manazeus@gmail.com>
2017-07-23 02:04:15 +0900

    modify index.html and manifest.json

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', (event) => {
+    console.info('install', event);
+
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    console.info('activate', event);
+
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    console.info('fetch', event);
+});
+
+self.addEventListener('push', (event) => {
+    console.info('push', event);
+
+    const message = event.data ? event.data.text() : '(-∀-)';
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification Title', {
+            body: message,
+            icon: '',
+            tag: 'push-notification-tag'
+        })
+    );
+});
